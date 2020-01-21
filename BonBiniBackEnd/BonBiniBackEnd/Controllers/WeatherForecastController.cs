82351b8160169b8903a16833ffd5c2e554cc104b
Arren <arren.van.dosselaer@hotmail.com>
2020-01-21 19:09:20 +0100

    upload project to github

---
+++ BonBiniBackEnd/BonBiniBackEnd/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.AspNetCore.Hosting;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using BonBiniBackEnd.Models;
+using System.Globalization;
+
+namespace BonBiniBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<Mail>  Get([FromBody] Mail mail)
+        {
+            CultureInfo ci = new CultureInfo("nl-BE");
+            CultureInfo.CurrentCulture = ci;
+            //var dtfi = CultureInfo.CurrentCulture.DateTimeFormat;
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Test Project", "arren.van.dosselaer@hotmail.com"));
+            message.To.Add(new MailboxAddress("arren test", "s095849@ap.be"));
+            message.Subject = "Reservering: " + mail.SenderName + " "+ mail.ReservationDate.ToLongDateString();
+            message.Body = new TextPart("plain")
+            {
+                Text =   mail.ReservationDate.ToLongDateString() + "\n" +
+                 "\n" + 
+                 mail.Comment + "\n"+
+                "\n" +
+                mail.SenderName + "\n" +
+                mail.PhoneNumber + "\n" +
+                mail.Email
+
+            };
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.live.com", 587, false);
+                client.Authenticate("arren.van.dosselaer@hotmail.com", "ayelar113");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return Ok(mail);
+        }
+    }
+}
---
+++ BonBiniBackEnd/BonBiniBackEnd/Models/Mail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonBiniBackEnd.Models
+{
+    public class Mail
+    {
+        [Required]
+        [StringLength(42)] // max 42, -> dus maximum email subject length is 78 characters, aangeraden https://tools.ietf.org/html/rfc5322#section-2.1.1
+        public string SenderName { get; set; }
+        [Required]
+        public string Comment { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DateTime ReservationDate { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
